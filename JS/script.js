425d6343fd97b77b10354f0e94eeefd9d1d0d99f
junior <lantoniodasilvaj@gmail.com>
2023-07-14 23:44:09 -0300

    secound commit

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,44 @@
-const url = "https://rickandmortyapi.com/api/character";
+const urlCharacters = "https://rickandmortyapi.com/api/character";
+const urlLocatios = "https://rickandmortyapi.com/api/location";
+const urlEpisodes = "https://rickandmortyapi.com/api/episode";
 const loadingPage = document.querySelector(".loader");
-const charactersContainer = document.querySelector("#charactersContainer");
+const cardsContainer = document.querySelector("#cardsContainer");
+const headerCharactersContainer = document.querySelector("#headerCharactersContainer");
 
-async function getAllCharacters() {
-  const response = await fetch(url); // response
-  const data = await response.json(); // converting to json
+async function getAll() {
+  const [responseChracter, responseLocations, responseEpisodes] = await Promise.all([
+    fetch(urlCharacters), // response characters
+    fetch(urlLocatios), //response locations
+    fetch(urlEpisodes) //response episodes 
+  ])
+  // converting to json
+  const dataCharacters = await responseChracter.json();
+  const dataLocations = await responseLocations.json();
+  const dataEpisodes = await responseEpisodes.json();  
+
+  console.log(dataCharacters, dataLocations, dataEpisodes)
   
-  loadingPage.classList.remove("loader"); // removing the loading
+  // removing the loading
+  loadingPage.classList.remove("loader"); 
   loadingPage.classList.add("hide"); 
-  console.log(data.results);
+
+  const createDiv = document.createElement("div");
+  createDiv.innerHTML = `
+    <h2>Characters</h2>
+  `;
+  headerCharactersContainer.appendChild(createDiv);
   
-  data.results.map((data) => {
+  dataCharacters.results.map((data) => {
 
     const divCharacterContainer = document.createElement("div");
     divCharacterContainer.classList.add("CharactersCard");
     divCharacterContainer.innerHTML = `
     <div class="imgContainer">
-      <img src="${data.image}" alt="image of ${data.name}" title="image of ${data.name}">
+      <img 
+        src="${data.image}" 
+        alt="${data.name} image" 
+        title="${data.name} image"
+      >
     </div>
     <div class="nameCharacterContainer">
       <h3 class="nameCharacter">${data.name}</h3>
@@ -28,9 +50,9 @@ async function getAllCharacters() {
       </ul>
     </div>
     `;
-    charactersContainer.appendChild(divCharacterContainer);
+    cardsContainer.appendChild(divCharacterContainer);
   
   });
 };
 
-getAllCharacters();
+getAll();
